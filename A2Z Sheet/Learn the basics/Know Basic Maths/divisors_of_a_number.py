6472383e036f312f2f852928172e6688cd109596
Soumen Saha <43337272+imsoumen@users.noreply.github.com>
2026-03-24 00:25:31 +0530

    divisors solution

---
+++ A2Z Sheet/Learn the basics/Know Basic Maths/divisors_of_a_number.py
@@ -0,0 +1,33 @@
+# Brute Force:
+
+class Solution:
+    def divisors(self, n): 
+        ans = []
+        for i in range(1,n+1):
+            if n%i == 0:
+                ans.append(i)
+        
+        return ans
+    
+# TC: O(N)
+# SC: O(sqrt(N))
+
+# Optimal
+import math
+class Solution:
+    def divisors(self, n):
+        ans = []
+
+        for i in range(1, math.sqrt(n)+1):
+            if n%i == 0:
+                ans.append(i)
+
+            if i != n // i:
+                ans.append(n // i)
+        
+        ans.sort()
+        
+        return ans
+
+# TC: O(sqrt(N)) + O(K*Log(K))
+# SC: O(sqrt(N))
